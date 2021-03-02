7abc108f9c65c66e02896e93b635a6e1759ba320
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-02 11:38:32 +0100

    add. snack 2

---
+++ snack2/js/script.js
@@ -0,0 +1,20 @@
+/*L’utente inserisce due parole in successione, con due
+prompt. Il software stampa prima la parola più corta, poi la
+parola più lunga*/
+
+
+var parola1 = prompt("inserisci la prima parola");
+
+var parola2 = prompt("inserisci la seconda parola");
+
+if (parola1.length > parola2.length){
+
+  console.log(parola1);
+  console.log(parola2);
+
+} else if (parola1.length < parola2.length){
+  console.log(parola2);
+  console.log(parola1);
+} else{
+  console.log( "le parole hanno la stessa lunghezza");
+}
