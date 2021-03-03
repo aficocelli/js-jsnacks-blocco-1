3fa3b0848e50f16dd243d66730df1fee6ac21655
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-03 13:06:34 +0100

    add. snack7

---
+++ snack7/js/script.js
@@ -0,0 +1,15 @@
+
+
+var numero = 2;
+
+
+for (i = 0; Math.pow(2, i) < 1000; i++ ){
+
+console.log(numero = Math.pow(2, i));
+
+
+
+
+
+
+}
---
+++ snack8/js/script.js
@@ -0,0 +1,1 @@
+var numero = parseInt(prompt("inserisci un numero di 4 cifre"));
