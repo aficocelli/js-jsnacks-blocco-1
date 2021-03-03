454520720a1d303c70a883fad03151682680282e
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-03 13:11:53 +0100

    add. snack8

--- snack8/js/script.js
+++ snack8/js/script.js
@@ -1,1 +1,11 @@
-var numero = parseInt(prompt("inserisci un numero di 4 cifre"));
+var numero = (prompt("inserisci un numero di 4 cifre"));
+
+var somma = 0
+
+for (var i = 0; i < numero.length; i++){
+
+  somma = somma + parseInt(numero[i]);
+
+
+}
+console.log(somma);
