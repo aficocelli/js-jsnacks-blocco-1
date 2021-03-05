e0cb392804ff339490183784099618f5d725e2fb
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-05 10:10:59 +0100

    fix snack 5

--- snack 5 - blocco 2/js/script.js
+++ snack 5 - blocco 2/js/script.js
@@ -4,22 +4,33 @@ fino a quando ne avrà tanti quanti l’altro.*/
 
 // 1 creo primo array
 
-var box1 = [1, 3, "bianchi", 6];
+var box2 = [1, 3, "bianchi", 6];
 
 // 2 creo secondo array
 
-var box2 = [2, 4, 5, "rossi", 4, 7, 8];
+var box1 = [2, 4, 5, "rossi", 4, 7, 8];
 
 // 3 aggiungi elementi casuali
 
 
-
+if (box1.length < box2.length){
 do {
 
   box1.push(Math.floor(Math.random() * 100));
 
 
 } while( box1.length < box2.length );
+
+} else if (box1.length > box2.length){
+
+  do {
+
+    box2.push(Math.floor(Math.random() * 100));
+
+
+  } while( box2.length < box1.length );
+
+}
 
 console.log(box1);
 
